d24c0b38cc884c10b86860da074b493ecdfacc38
razaakomal <komal.raza@leoatech.com>
2026-06-01 15:44:17 +0500

    fix: enforce ssl rejectUnauthorized for connectionString in knexfile

--- knexfile.js
+++ knexfile.js
@@ -25,7 +25,10 @@ module.exports = {
   production: {
     client: "postgresql",
    connection: process.env.DATABASE_URL
-      ? { connectionString: process.env.DATABASE_URL }
+      ? { 
+          connectionString: process.env.DATABASE_URL,
+          ssl: { rejectUnauthorized: false }
+        }
       : {
           host: process.env.DB_HOST,
           port: parseInt(process.env.DB_PORT || "5432", 10),
