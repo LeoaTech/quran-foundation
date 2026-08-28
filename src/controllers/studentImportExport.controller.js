81046e8446350eab1b76b9051d8a45de0f7b8421
razaakomal <komal.raza@leoatech.com>
2026-08-28 12:39:19 +0500

    [QFL-27]- feat(backend:service[studentImportExport]): add importStudents and getImportTemplate functions for student data handling

--- src/controllers/studentImportExport.controller.js
+++ src/controllers/studentImportExport.controller.js
@@ -17,7 +17,38 @@ async function exportStudents(req, res, next) {
   }
 }
 
+async function importStudents(req, res, next) {
+  try {
+    const { center_id } = req.body || {};
+    const fileBuffer = req.file?.buffer;
+
+    const summary = await service.importStudentsData({
+      user: req.user,
+      centerId: center_id || req.query.center_id,
+      fileBuffer,
+    });
+
+    res.json(summary);
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function getImportTemplate(req, res, next) {
+  try {
+    const { format } = req.query;
+    const result = await service.getImportTemplate({ format });
+
+    res.setHeader('Content-Type', result.contentType);
+    res.setHeader('Content-Disposition', `attachment; filename="${result.filename}"`);
+    res.send(result.buffer);
+  } catch (err) {
+    next(err);
+  }
+}
 
 module.exports = {
-  exportStudents
+  exportStudents,
+  importStudents,
+  getImportTemplate,
 };
